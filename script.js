72febde2871981e9cc3f12ba61cf67557cf80ccf
Ramón Huidobro <hola@ramonh.dev>
2023-09-13 12:31:56 +0200

    create site

---
+++ script.js
@@ -0,0 +1,14 @@
+// Create a large JavaScript file (for demonstration purposes)
+const largeArray = new Array(1000000).fill("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+
+// Simulate a long-running operation
+function simulateLoad() {
+    console.log("Start loading large.js");
+    for (let i = 0; i < largeArray.length; i++) {
+        // Simulate some processing
+    }
+    console.log("Finished loading large.js");
+}
+
+// Call the simulation function when the page loads
+window.addEventListener("load", simulateLoad);
